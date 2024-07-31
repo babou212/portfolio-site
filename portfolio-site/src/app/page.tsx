1821e8915161c5ba3fcf3a8700b45bc32c4511d3
Dylan Cree <dylanpab1@gmail.com>
2024-07-31 14:48:35 +0100

    changed photo sizing on front page

--- portfolio-site/src/app/page.tsx
+++ portfolio-site/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center">
         {imageCall.map((image) => (
-            <div key={Math.random()} className="relative h-80 w-80">
+            <div key={Math.random()} className="relative h-96 w-96">
                 <Image src={image.image} alt={image.title}
                   fill
                   quality={80}
